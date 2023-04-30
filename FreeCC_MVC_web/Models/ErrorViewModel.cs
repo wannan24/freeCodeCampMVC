21c78874e59d2777dbd9fa489b3c36f16b335c02
wannan24 <marwanwannan24@gmail.com>
2023-04-30 05:03:32 +0300

    init project

---
+++ FreeCC_MVC_web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreeCC_MVC_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
